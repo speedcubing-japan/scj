3800a2fc20af6920258a38f189859ee7156efffe
Sinpei Araki <speedsolve@gmail.com>
2021-07-26 01:51:32 +0900

    開発と本番環境を間違えやすいので色を変更

--- python/src/app/context_processors.py
+++ python/src/app/context_processors.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from app.defines.competitor import Status as CompetitorStatus
 from app.defines.fee import PayTypeEn as FeePayType
 from app.defines.fee import CalcTypeEn as FeeCalcType
@@ -6,6 +7,7 @@ from app.defines.competition import Type as CompetitionType
 
 def context_processor(req):
     return {
+        "is_debug": settings.DEBUG,
         "competitor_status": CompetitorStatus.__members__,
         "fee_pay_type": FeePayType.__members__,
         "fee_calc_type": FeeCalcType.__members__,
