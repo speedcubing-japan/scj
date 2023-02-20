2d6ad4b8faa222b0cb3f689c879877577b432c9c
Sinpei Araki <speedsolve@gmail.com>
2023-02-20 19:23:53 +0900

    Stripe Connect連携解除コマンド追加

---
+++ python/src/app/management/commands/deauthorization.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from app.models import Person
+from django.conf import settings
+import stripe
+
+
+class Command(BaseCommand):
+    help = "deauthorize stripe connect"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--person_id", nargs="?", default=0, type=int)
+
+    def handle(self, *args, **options):
+        if options["person_id"] is None or options["person_id"] == 0:
+            print("person_id is not assigned.")
+            exit()
+
+        person = Person.objects.get(pk=options["person_id"])
+
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.OAuth.deauthorize(
+            client_id=settings.STRIPE_CLIENT_ID, stripe_user_id=person.stripe_user_id
+        )
