85f85e1dacfa7cdd7916ea1d351905ba9b0bbb1c
Kenya Yahata <ky12228121@gmail.com>
2024-04-07 06:03:25 +0000

    管理画面の編集がuser_idのままだったのde修正

--- python/src/app/views/scj/admin/person_edit.py
+++ python/src/app/views/scj/admin/person_edit.py
@@ -11,8 +11,8 @@ class AdminPersonEdit(AdminBase, TemplateView):
     form_class = PersonEditForm
 
     def get(self, request, **kwargs):
-        user_id = kwargs.get("user_id")
-        person = Person.objects.get(id=user_id)
+        scj_id = kwargs.get("scj_id")
+        person = Person.objects.get(id=scj_id)
         person_dict = person.__dict__
         person_dict["is_active"] = person.user.is_active
         form = PersonEditForm(initial=person_dict)
@@ -22,14 +22,15 @@ class AdminPersonEdit(AdminBase, TemplateView):
         return self.form_valid(request.POST, request, **kwargs)
 
     def form_valid(self, form, request, **kwargs):
-        person = Person.objects.get(id=kwargs["user_id"])
+        person = Person.objects.get(id=kwargs["scj_id"])
         save_form = PersonEditForm(form, instance=person)
         if not save_form.is_valid():
             return render(
                 request, "app/scj/admin/person_edit.html", dict(form=save_form)
             )
         save_form.save()
-        user = User.objects.get(id=kwargs["user_id"])
+        user_id = person.user.id
+        user = User.objects.get(id=user_id)
         user.is_active = save_form.cleaned_data["is_active"]
         user.save()
         self.request.session["notification"] = Notification.PERSON_INFO_CHANGE
